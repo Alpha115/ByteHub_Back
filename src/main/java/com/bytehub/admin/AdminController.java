94a4285e0341d164f712a7ac4eb68da3d387158d
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-07-04 16:40:38 +0900

    [관리자]퇴사자 처리
    boolean toggle 방식

--- src/main/java/com/bytehub/admin/AdminController.java
+++ src/main/java/com/bytehub/admin/AdminController.java
@@ -1,5 +1,31 @@
 package com.bytehub.admin;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
 
+	private final AdminService service;
+	Map<String, Object> resp=null;
+	
+	// 관리자의 아이디로 직원을 퇴사처리하는 함수입니다.
+	@GetMapping("/withdraw/{id}")
+	public Map<String, Object> withdraw(@PathVariable String id){
+		resp=new HashMap<String, Object>();
+		resp.put("success", service.withdraw(id));
+		return resp;
+	}
+	
 }
--- src/main/java/com/bytehub/admin/AdminService.java
+++ src/main/java/com/bytehub/admin/AdminService.java
@@ -1,5 +1,20 @@
 package com.bytehub.admin;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class AdminService {
+
+	private final AdminDAO dao;
+
+	public boolean withdraw(String id) {
+		int row = dao.withdraw(id);
+		return row > 0;
+	}
 
 }
