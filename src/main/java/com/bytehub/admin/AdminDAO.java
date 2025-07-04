15c40cbfe005865ad8677f48601bee5d1006fbc5
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-07-04 17:02:25 +0900

    회원 리스트 조회 완료

--- src/main/java/com/bytehub/admin/AdminDAO.java
+++ src/main/java/com/bytehub/admin/AdminDAO.java
@@ -1,5 +1,8 @@
 package com.bytehub.admin;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
@@ -8,5 +11,10 @@ public interface AdminDAO {
 	
 	// 관리자의 아이디로 직원을 퇴사처리하는 함수입니다. toggle 방식
 	int withdraw(String id);
+
+	// 관리자의 아이디로 직원의 리스트(MemberDTO, array)를 불러오는 함수입니다.
+	ArrayList<Map<String, Object>> list();
+	
+	// 부서번호/직급번호 => 부서명, 직급명으로 변환
 
 }
