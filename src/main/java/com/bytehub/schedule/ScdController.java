b749715c6f7df8563dc0400114bd40e11c02c158
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-07-10 18:07:32 +0900

    플젝 정렬기능
    앗빼먹었다

--- src/main/java/com/bytehub/schedule/ScdController.java
+++ src/main/java/com/bytehub/schedule/ScdController.java
@@ -1,5 +1,15 @@
 package com.bytehub.schedule;
 
-public class ScdController {
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+@RequestMapping("/scd")
+public class ScdController {
+	
 }
