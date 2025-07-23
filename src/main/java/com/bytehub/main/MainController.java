08f472151e917e0222d99016d03bfb63a418beba
Alpha <118242780+Alpha115@users.noreply.github.com>
2025-07-23 17:31:23 +0900

    로그인막기

--- src/main/java/com/bytehub/main/MainController.java
+++ src/main/java/com/bytehub/main/MainController.java
@@ -1,34 +1,21 @@
 package com.bytehub.main;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import com.bytehub.member.MemberService;
 
 @Slf4j
 @CrossOrigin
 @RestController
-@RequiredArgsConstructor
 public class MainController {
 
-	// ▼ json 형식의 response
-	Map<String, Object> resp = null;
+	@RequestMapping("/")
+	public void home() {
 
-	private final MemberService service;
+		log.info("집이아닙니다");
 
-	@GetMapping("/")
-	public Map<String, Object> home() {
-		resp=new HashMap<String, Object>();
-		resp.put("msg", "메인 페이지");
-		return resp;
 	}
-	// 확인
-
 
 }
--- src/main/java/com/bytehub/utils/LoginChecker.java
+++ src/main/java/com/bytehub/utils/LoginChecker.java
@@ -13,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LoginChecker implements HandlerInterceptor {
 	
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
@@ -24,7 +25,8 @@ public class LoginChecker implements HandlerInterceptor {
 		if (loginId == null || loginId.equals("")) {	// 로긴이 안돼있다면 퉤 
 			pass = false;
 			String context=request.getContextPath();	// context 경로 추출
-			response.sendRedirect(context);	// Context 경로도 같이줘야함, context경로가 없으면 걍 /가 들어갈 것이다
+			response.sendRedirect("http://localhost:3000/");	// Context 경로도 같이줘야함, context경로가 없으면 걍 /가 들어갈 것이다
+			// 근데 어카냐...
 		}
 		return pass;
 	}
