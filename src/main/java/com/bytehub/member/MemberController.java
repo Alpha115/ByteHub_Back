bd0cbf1311df555677a75c1231de97ac10b7b086
zzztkddbszzz <dltkddbsfl@gmail.com>
2025-07-30 12:13:24 +0900

    dept_idx

--- src/main/java/com/bytehub/member/MemberController.java
+++ src/main/java/com/bytehub/member/MemberController.java
@@ -32,7 +32,7 @@ public class MemberController {
         try {
             // dept_idx와 lv_idx가 없거나 0이면 기본값 설정
             if (param.get("dept_idx") == null || (Integer) param.get("dept_idx") == 0) {
-                param.put("dept_idx", 11); // 기본 부서 ID
+                param.put("dept_idx", 99); // 기본 부서 ID
             }
             if (param.get("lv_idx") == null || (Integer) param.get("lv_idx") == 0) {
                 param.put("lv_idx", 7); // 기본 직급 ID
